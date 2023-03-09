ec687d0aa82edbec622674c24b518d2fd0d85745
kinxyos <kinjalkt3@gmail.com>
2023-03-09 18:30:45 +0530

    QRCode Generator added

---
+++ QRCodeGenerator/main.py
@@ -0,0 +1,23 @@
+import qrcode as qr
+import random
+import eel
+
+eel.init('web')
+
+@eel.expose()
+def makeqr(link):
+    img = qr.make(link)
+    link = link.lstrip("https://www.")
+    link = link.lstrip("http://www.")
+    print(link)
+    print("\n")
+    link2 = link.partition("/")
+    link2 = link2[0]
+    link2 = link2.rstrip(".com")
+    link2 = link2.rstrip(".edu")
+    link2 = link2.rstrip(".org")
+    num = random.randrange(1,9999)
+    img.save(f"{link2}_QR{num}.png")
+    print(f"'{link2}_QR{num}.png' has been saved!")
+
+eel.start('index.html', size=(856, 554))
